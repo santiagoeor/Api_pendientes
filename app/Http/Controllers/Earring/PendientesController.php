1403273cd2e4f3693ffa0767489dec33742406ff
Santiago Orozco <everardoorozcocarmona@gmail.com>
2023-04-30 21:07:22 -0500

    se creo las tablas en bd, y sese creo el archivo model y de controller de categorias y pendientes

---
+++ app/Http/Controllers/Earring/PendientesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Earring;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PendientesController extends Controller
+{
+    //
+}
