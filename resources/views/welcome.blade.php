58c942196232a55ca9a438ed0a36e88b9e5779de
Santiago Orozco <everardoorozcocarmona@gmail.com>
2023-05-03 14:50:55 -0500

    pruebas de integracion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Api Laravel</title>
+        <title>Api Laravel v1</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
